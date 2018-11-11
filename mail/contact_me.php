4859708f8968ec551e0b9727030f134fcdbfe1c4
Gergely Szabo <geeryszabo@gmail.com>
2018-11-12 00:37:49 +0100

    attempt to fix mail sending

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -21,12 +21,12 @@ try {
     //Server settings
     $mail->SMTPDebug = 2;                                 // Enable verbose debug output
     $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'mail.gandi.net';  // Specify main and backup SMTP servers
+    $mail->Host = 'smtp.mail.eu-west-1.awsapps.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
     $mail->Username = 'noreply@gergely-szabo.com';                 // SMTP username
     $mail->Password = 'M9HR7NJ!2F4fRK*&m6dQ@p96';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 587;                                    // TCP port to connect to
+    $mail->Port = 465;                                    // TCP port to connect to
 
     //Recipients
     $mail->setFrom('noreply@gergely-szabo.com', 'Mailer');
