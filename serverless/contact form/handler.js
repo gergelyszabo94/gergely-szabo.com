b8f9d93be96d36f12fc71c7eacc2839941b98cf2
Gergely Szabo <geeryszaabo@gmail.com>
2019-11-22 00:14:22 +0100

    allows alternative origins for CORS

--- serverless/contact form/handler.js
+++ serverless/contact form/handler.js
@@ -3,15 +3,15 @@ const request = require('request');
 const ses = new aws.SES();
 const source_email = process.env.SOURCE_EMAIL;
 const destination_email = process.env.DESTINATION_EMAIL;
-const myDomain = process.env.DOMAIN;
 const hcaptcha_secret = process.env.HCAPTCHA_SECRET;
-const hcaptcha_site_key = process.env.HCAPTCHA_SITE_KEY;
+const allowed_origins = ['https://www.gergely-szabo.com', 'https://gergely-szabo.com', 'http://development.gergely-szabo.com', 'https://development.gergely-szabo.com'];
 
-function generateResponse (code, payload) {
+function generateResponse (code, origin, payload) {
+  origin = origin === null ? allowed_origins[0] : origin;
   return {
     statusCode: code,
     headers: {
-      'Access-Control-Allow-Origin': myDomain,
+      'Access-Control-Allow-Origin': origin,
       'Access-Control-Allow-Headers': 'x-requested-with',
       'Access-Control-Allow-Credentials': true
     },
@@ -19,12 +19,13 @@ function generateResponse (code, payload) {
   }
 }
 
-function generateError (code, err) {
+function generateError (code, origin, err) {
   console.log(err);
+  origin = origin === null ? allowed_origins[0] : origin;
   return {
     statusCode: code,
     headers: {
-      'Access-Control-Allow-Origin': myDomain,
+      'Access-Control-Allow-Origin': origin,
       'Access-Control-Allow-Headers': 'x-requested-with',
       'Access-Control-Allow-Credentials': true
     },
@@ -62,6 +63,8 @@ module.exports.send = async (event) => {
   try {
     const emailParams = generateEmailParams(event.body);
     const hCaptchaClientResponseToken = JSON.parse(event.body).hCaptcha;
+    const origin = event.headers.origin;
+    if (!allowed_origins.includes(origin)) return generateError(403, null, {message: 'CORS'});
 
     if (hCaptchaClientResponseToken !== undefined && hCaptchaClientResponseToken !== '') {
       return new Promise((resolve, reject) => {
@@ -73,18 +76,18 @@ module.exports.send = async (event) => {
         }, async (err, httpResponse, body) => {
           if (err) {
             console.log(err);
-            reject(generateError(403, {message: 'Could not verify hCaptcha'}));
+            reject(generateError(403, origin, {message: 'Could not verify hCaptcha'}));
           } else {
             console.log(body);
             const data = await ses.sendEmail(emailParams).promise();
-            resolve(generateResponse(200, data));
+            resolve(generateResponse(200, origin, data));
           }
         });
       });
     }
-    else return generateError(403, {message: 'Captcha token missing'});
+    else return generateError(403, origin, {message: 'Captcha token missing'});
 
   } catch (err) {
-    return generateError(500, err)
+    return generateError(500, null, err)
   }
 };
