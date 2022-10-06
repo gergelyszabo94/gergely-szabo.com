698f8bff62ab709c8be1846d2b778e6e3e1962db
Gergely Szabo <gergely@gergely-szabo.com>
2022-10-06 16:27:37 +0200

    profile header refactored

--- astro/public/js/background.js
+++ astro/public/js/background.js
@@ -1,38 +1,24 @@
-/**
- * Created by Gergely on 9/16/2017.
- */
+const images = [
+  "brugessquare.jpg",
+  "primrosehillview.jpg",
+  "tinystonehengeandlake.jpg",
+];
 
-$(document).ready(function () {
-// Changing background images
-    var images = [
-        "/img/background/brugessquare.jpg",
-        "/img/background/primrosehillview.jpg",
-        "/img/background/tinystonehengeandlake.jpg"
-    ]
+const credits = [
+  "Gergely Szabo, July of 2016 <br> The Market Square, Bruges, Belgium",
+  "Gergely Szabo, August of 2017 <br> Primrose Hill Regent's Park, London, United Kingdom",
+  "Gergely Szabo, July of 2017 <br> Weißsee, High Tauern, Austria",
+];
 
-    var imageHead = document.getElementById('profile');
+const imageHead = document.getElementById('profile');
+const photoCreditTextEl = document.getElementById('photo-credit');
 
+let i = 0;
 
-    var credits = [
-        "Gergely Szabo, July of 2016 <br> The Market Square, Bruges, Belgium",
-        "Gergely Szabo, August of 2017 <br> Primrose Hill Regent's Park, London, United Kingdom",
-        "Gergely Szabo, July of 2017 <br> Weißsee, High Tauern, Austria"
-    ]
-
-    var photoCreditText = document.getElementById('photo-credit');
-
-    var i = 0;
-    setInterval(function () {
-        imageHead.style.backgroundImage = "url(" + images[i] + ")";
-        photoCreditText.innerHTML = credits[i];
-        i = i + 1;
-        if (i == images.length) {
-            i = 0;
-        }
-    }, 10000);
-
-//Loading gif fadeout after page is loaded, gif from https://loading.io/
-//     $(window).on('load', function () {
-//         $(".se-pre-con").fadeOut("slow");
-//     });
-});
+setInterval(() => {
+  if (imageHead && photoCreditTextEl) {
+    imageHead.style.backgroundImage = `url(/img/background/${images[i]})`;
+    photoCreditTextEl.innerHTML = credits[i];
+    i = i == images.length ? 0 : i += 1;
+  }
+}, 10000);
