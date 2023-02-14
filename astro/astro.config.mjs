05c1b16d01703b3964dfe524decb14a28f47b135
Gergely Szabo <gergely@gergely-szabo.com>
2023-02-14 21:11:45 +0100

    sitemap

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  site: 'https://www.gergely-szabo.com/'
+  site: 'https://www.gergely-szabo.com/',
+  integrations: [sitemap()]
 });
